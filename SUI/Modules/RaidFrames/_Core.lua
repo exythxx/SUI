45eecfbd787396d79ba0d7bd5b97b7ba8c957c0f
Exyth1 <josef.sedlacek33@gmail.com>
2023-05-04 00:59:43 +0200

    Raidframe texture flickering fix

--- SUI/Modules/RaidFrames/_Core.lua
+++ SUI/Modules/RaidFrames/_Core.lua
@@ -9,61 +9,22 @@ function Module:OnEnable()
 		h:SetMinMaxValues(1, 200)
 		w:SetMinMaxValues(1, 200)
 
-		--BLACK SKIN
-		local function update()
-			local i, bar = 1
-			repeat
-			bar = _G["CompactRaidFrame" .. i .. "HealthBar"]
-			local rbar = _G["CompactRaidFrame" .. i .. "PowerBar"]
-			local Divider = _G["CompactRaidFrame" .. i .. "HorizDivider"]
-			local vleftseparator = _G["CompactRaidFrame" .. i .. "VertLeftBorder"]
-			local vrightseparator = _G["CompactRaidFrame" .. i .. "VertRightBorder"]
-			local htopseparator = _G["CompactRaidFrame" .. i .. "HorizTopBorder"]
-			local hbotseparator = _G["CompactRaidFrame" .. i .. "HorizBottomBorder"]
-			local bordertopleft = _G["CompactRaidFrameContainerBorderFrameBorderTopLeft"]
-			local bordertop = _G["CompactRaidFrameContainerBorderFrameBorderTop"]
-			local bordertopright = _G["CompactRaidFrameContainerBorderFrameBorderTopRight"]
-			local borderleft = _G["CompactRaidFrameContainerBorderFrameBorderLeft"]
-			local borderright = _G["CompactRaidFrameContainerBorderFrameBorderRight"]
-			local borderbottomleft = _G["CompactRaidFrameContainerBorderFrameBorderBottomLeft"]
-			local borderbottom = _G["CompactRaidFrameContainerBorderFrameBorderBottom"]
-			local borderbottomright = _G["CompactRaidFrameContainerBorderFrameBorderBottomRight"]
-			if bar then
-				--STATUSBAR
-				if (db.texture ~= 'Default') then
-					bar:SetStatusBarTexture(db.texture)
-					bar:GetStatusBarTexture():SetDrawLayer("BORDER")
-					rbar:SetStatusBarTexture(db.texture)
-					rbar:GetStatusBarTexture():SetDrawLayer("BORDER")
+		local function updateTextures(self)
+			if self:IsForbidden() then return end
+			local name = self:GetName()
+			if name and name:match("^Compact") then
+				if db.texture ~= [[Interface\Default]] then
+						self.healthBar:SetStatusBarTexture(db.texture)
+						self.healthBar:GetStatusBarTexture():SetDrawLayer("BORDER")
+						self.powerBar:SetStatusBarTexture(db.texture)
+						self.powerBar:GetStatusBarTexture():SetDrawLayer("BORDER")
 				end
-				--DARK
-				bordertopleft:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\RaidFrames\\RaidBorder-UpperLeft")
-				bordertop:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\RaidFrames\\RaidBorder-UpperMiddle")
-				bordertopright:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\RaidFrames\\RaidBorder-UpperRight")
-				borderleft:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\RaidFrames\\RaidBorder-Left")
-				borderright:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\RaidFrames\\RaidBorder-Right")
-				borderbottomleft:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\RaidFrames\\RaidBorder-BottomLeft")
-				borderbottom:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\RaidFrames\\RaidBorder-BottomMiddle")
-				borderbottomright:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\RaidFrames\\RaidBorder-BottomRight")
-				vleftseparator:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\RaidFrames\\Raid-VSeparator")
-				vrightseparator:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\RaidFrames\\Raid-VSeparator")
-				htopseparator:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\RaidFrames\\Raid-HSeparator")
-				hbotseparator:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\RaidFrames\\Raid-HSeparator")
-
-				Divider:SetVertexColor(.3, .3, .3)
-			end
-			i = i + 1
-			until not bar
-		end
-
-		if (SUI:Color()) then
-			if CompactRaidFrameContainer_AddUnitFrame then
-				--self:UnregisterAllEvents()
-				hooksecurefunc("CompactRaidFrameContainer_AddUnitFrame", update)
-				hooksecurefunc("CompactUnitFrame_UpdateAll", update)
-				update()
 			end
 		end
+
+		hooksecurefunc("CompactUnitFrame_UpdateAll", function(self)
+			updateTextures(self)
+		end)
 
 		--RAID BUFFS
 		if (db.partybuffs) == true then
